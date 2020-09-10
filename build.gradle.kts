672eb167ffad9d5f93332ab6640513b1d94040e4
Masahiro Kitagawa <masahiro_kitagawa@nidek.co.jp>
2020-09-10 18:16:41 +0900

    Replace classes from icedtea6 with image-codec-jpeg dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,13 @@ version = "1.1.3.0"
 
 dependencies {
     implementation(files("lib/mlibwrapper-jai-1.1.3.jar"))
+    implementation("com.github.ktgw0316:image-codec-jpeg:6.1.13.1")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    maven("https://jitpack.io")
 }
